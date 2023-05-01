e051b6d117943032072145315fbd161cd0911845
Romelo-S <rsb3x@umsystem.edu>
2023-05-01 12:44:29 -0500

    Finished Class tree.
    Need to make tree smaller

--- ClassificationTree.py
+++ ClassificationTree.py
@@ -11,6 +11,9 @@ class main:
     train = trainingData(numbs)
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(train,labels)
+    fig = plt.figure(figsize=(25,20))
+    tree.plot_tree(clf,feature_names=["Cost","Maintence cost","Doors","Seat","Luggage Size","Safety"],class_names=["unacc","acc","good","vgood"],filled=True)
+    fig.savefig("CarDecisionTree2.png")
     myTree = tree.export_text(clf)
     print(myTree)
 #What should be our training vs testing samples. The data sheet is like sorted so how to do the training vs Testing samples
